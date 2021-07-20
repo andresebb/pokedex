ac80e103c0246d95c5c1497f67f6bb9d54d7175f
Andres Betancourt <aebb001@gmail.com>
2021-07-19 20:09:21 -0500

    Config Inicial y diseno basico del Home

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Image, Text, View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {styles} from '../theme/AppTheme';
+
+export const HomeScreen = () => {
+  const {top} = useSafeAreaInsets();
+
+  return (
+    <View>
+      <Image
+        source={require('../assets/pokebola.png')}
+        style={styles.pokebolaBG}
+      />
+
+      <Text style={{...styles.title, top: top + 20, ...styles.globalMargin}}>
+        Pokedex
+      </Text>
+    </View>
+  );
+};
